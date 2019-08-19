3f8a060d615715df21745e9ac769f77410a63c2c
admin <root@localhost>
2019-08-19 14:39:52 +0000

    testing

--- Recon_Playbook.py
+++ Recon_Playbook.py
@@ -51,7 +51,7 @@ def whois_domain_1(action=None, success=None, container=None, results=None, hand
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("whois domain", parameters=parameters, assets=['whois'], callback=join_format_1, name="whois_domain_1")
+    phantom.act("whois domain", parameters=parameters, assets=['whois'], name="whois_domain_1")
 
     return
 
@@ -72,7 +72,7 @@ def whois_ip_1(action=None, success=None, container=None, results=None, handle=N
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("whois ip", parameters=parameters, assets=['whois'], callback=join_format_1, name="whois_ip_1")
+    phantom.act("whois ip", parameters=parameters, assets=['whois'], name="whois_ip_1")
 
     return
 
@@ -93,7 +93,7 @@ def file_reputation_1(action=None, success=None, container=None, results=None, h
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("file reputation", parameters=parameters, assets=['virustotal_api'], callback=join_format_1, name="file_reputation_1")
+    phantom.act("file reputation", parameters=parameters, assets=['virustotal_api'], callback=filter_3, name="file_reputation_1")
 
     return
 
@@ -102,18 +102,16 @@ def format_1(action=None, success=None, container=None, results=None, handle=Non
     
     template = """File Reputation = {0}
 
-Whois IP = {1}
+Whois IP = 
 
-Whois Domain = {2}
+Whois Domain = 
 
-URL Reputation = {3}"""
+URL Reputation = {1}"""
 
     # parameter list for template variable replacement
     parameters = [
-        "file_reputation_1:action_result.data.*.positives",
-        "whois_ip_1:action_result.message",
-        "whois_domain_1:action_result.message",
-        "file_reputation_1:action_result.status",
+        "detonate_file_1:action_result.status",
+        "url_reputation_1:action_result.parameter.url",
     ]
 
     phantom.format(container=container, template=template, parameters=parameters, name="format_1")
@@ -126,7 +124,7 @@ def join_format_1(action=None, success=None, container=None, results=None, handl
     phantom.debug('join_format_1() called')
 
     # check if all connected incoming actions are done i.e. have succeeded or failed
-    if phantom.actions_done([ 'file_reputation_1', 'whois_domain_1', 'whois_ip_1', 'url_reputation_1' ]):
+    if phantom.actions_done([ 'detonate_url_1', 'url_reputation_1', 'file_reputation_1', 'detonate_file_1' ]):
         
         # call connected block "format_1"
         format_1(container=container, handle=handle)
@@ -150,7 +148,7 @@ def url_reputation_1(action=None, success=None, container=None, results=None, ha
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("url reputation", parameters=parameters, assets=['phishtank'], callback=join_format_1, name="url_reputation_1")
+    phantom.act("url reputation", parameters=parameters, assets=['virustotal_api'], callback=filter_5, name="url_reputation_1")
 
     return
 
@@ -176,7 +174,205 @@ def prompt_1(action=None, success=None, container=None, results=None, handle=Non
         },
     ]
 
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types)
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types, callback=exmerge)
+
+    return
+
+def filter_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_3() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["file_reputation_1:action_result.data.*.positives", ">=", "5"],
+        ],
+        name="filter_3:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        join_hunt_file(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["file_reputation_1:action_result.data.*.positives", "<=", "4"],
+        ],
+        name="filter_3:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        detonate_file_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def detonate_file_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('detonate_file_1() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'detonate_file_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.fileHashSha1', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'detonate_file_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'vault_id': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("detonate file", parameters=parameters, assets=['virustotal_api'], callback=join_hunt_file, name="detonate_file_1")
+
+    return
+
+def hunt_file(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('hunt_file() called')
+    input_parameter_0 = ""
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+    join_format_1(container=container)
+
+    return
+
+def join_hunt_file(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('join_hunt_file() called')
+
+    # check if all connected incoming actions are done i.e. have succeeded or failed
+    if phantom.actions_done([ 'file_reputation_1', 'detonate_file_1' ]):
+        
+        # call connected block "hunt_file"
+        hunt_file(container=container, handle=handle)
+    
+    return
+
+def filter_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_5() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_1:action_result.data.*.positives", ">=", "1"],
+        ],
+        name="filter_5:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        join_block_url(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_1:action_result.data.*.positives", "<", "1"],
+        ],
+        name="filter_5:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        detonate_url_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def detonate_url_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('detonate_url_1() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'detonate_url_1' call
+    inputs_data_1 = phantom.collect2(container=container, datapath=['url_reputation_1:artifact:*.cef.requestURL', 'url_reputation_1:artifact:*.id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'detonate_url_1' call
+    for inputs_item_1 in inputs_data_1:
+        if inputs_item_1[0]:
+            parameters.append({
+                'url': inputs_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': inputs_item_1[1]},
+            })
+
+    phantom.act("detonate url", parameters=parameters, assets=['virustotal_api'], callback=filter_6, name="detonate_url_1")
+
+    return
+
+def filter_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_6() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["detonate_url_1:action_result.data.*.positives", ">=", 50],
+        ],
+        name="filter_6:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        join_block_url(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def block_url(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('block_url() called')
+    input_parameter_0 = ""
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+    join_format_1(container=container)
+
+    return
+
+def join_block_url(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('join_block_url() called')
+
+    # check if all connected incoming actions are done i.e. have succeeded or failed
+    if phantom.actions_done([ 'detonate_url_1', 'url_reputation_1' ]):
+        
+        # call connected block "block_url"
+        block_url(container=container, handle=handle)
+    
+    return
+
+def exmerge(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('exmerge() called')
+    input_parameter_0 = ""
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
 
