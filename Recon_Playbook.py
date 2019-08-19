7beeab8fae811fa8a3d8b02fcca55975915165ad
admin <root@localhost>
2019-08-19 18:44:38 +0000

    Saved: testing

--- Recon_Playbook.py
+++ Recon_Playbook.py
@@ -23,50 +23,8 @@ def on_start(container):
     # call 'file_reputation_1' block
     file_reputation_1(container=container)
 
-    # call 'whois_domain_1' block
-    whois_domain_1(container=container)
-
-    return
-
-def whois_domain_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('whois_domain_1() called')
-
-    # collect data for 'whois_domain_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationDnsDomain', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'whois_domain_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'domain': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("whois domain", parameters=parameters, assets=['whois'], name="whois_domain_1")
-
-    return
-
-def whois_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('whois_ip_1() called')
-
-    # collect data for 'whois_ip_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationAddress', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'whois_ip_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'ip': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("whois ip", parameters=parameters, assets=['whois'], name="whois_ip_1")
+    # call 'url_reputation_2' block
+    url_reputation_2(container=container)
 
     return
 
@@ -88,99 +46,6 @@ def file_reputation_1(action=None, success=None, container=None, results=None, h
             })
 
     phantom.act("file reputation", parameters=parameters, assets=['virustotal_api'], callback=File_Filter, name="file_reputation_1")
-
-    return
-
-def url_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('url_reputation_1() called')
-
-    # collect data for 'url_reputation_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.requestURL', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'url_reputation_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'url': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("url reputation", parameters=parameters, assets=['virustotal_api'], callback=filter_5, name="url_reputation_1")
-
-    return
-
-def filter_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('filter_5() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["url_reputation_1:action_result.data.*.positives", ">=", "1"],
-        ],
-        name="filter_5:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        pass
-
-    # collect filtered artifact ids for 'if' condition 2
-    matched_artifacts_2, matched_results_2 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["url_reputation_1:action_result.data.*.positives", "<", "1"],
-        ],
-        name="filter_5:condition_2")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_2 or matched_results_2:
-        detonate_url_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
-
-    return
-
-def detonate_url_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('detonate_url_1() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'detonate_url_1' call
-    inputs_data_1 = phantom.collect2(container=container, datapath=['url_reputation_1:artifact:*.cef.requestURL', 'url_reputation_1:artifact:*.id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'detonate_url_1' call
-    for inputs_item_1 in inputs_data_1:
-        if inputs_item_1[0]:
-            parameters.append({
-                'url': inputs_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': inputs_item_1[1]},
-            })
-
-    phantom.act("detonate url", parameters=parameters, assets=['virustotal_api'], callback=filter_6, name="detonate_url_1")
-
-    return
-
-def filter_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('filter_6() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["detonate_url_1:action_result.data.*.positives", ">=", 50],
-        ],
-        name="filter_6:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        pass
 
     return
 
@@ -370,6 +235,120 @@ def filter_9(action=None, success=None, container=None, results=None, handle=Non
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         pass
+
+    return
+
+def url_reputation_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('url_reputation_2() called')
+
+    # collect data for 'url_reputation_2' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.requestURL', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'url_reputation_2' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'url': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("url reputation", parameters=parameters, assets=['virustotal_api'], callback=filter_10, name="url_reputation_2")
+
+    return
+
+def filter_10(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_10() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_2:action_result.data.*.positives", ">", "1"],
+        ],
+        name="filter_10:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        prompt_6(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_2:action_result.data.*.positives", "<=", 0],
+        ],
+        name="filter_10:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        prompt_7(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def prompt_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('prompt_6() called')
+    
+    # set user and message variables for phantom.prompt call
+    user = "admin"
+    message = """testing positive
+{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "url_reputation_2:action_result.data.*.positives",
+    ]
+
+    #responses:
+    response_types = [
+        {
+            "prompt": "",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No",
+                ]
+            },
+        },
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_6", parameters=parameters, response_types=response_types)
+
+    return
+
+def prompt_7(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('prompt_7() called')
+    
+    # set user and message variables for phantom.prompt call
+    user = "admin"
+    message = """detonate url
+{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_10:condition_2:url_reputation_2:action_result.data.*.positives",
+    ]
+
+    #responses:
+    response_types = [
+        {
+            "prompt": "",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No",
+                ]
+            },
+        },
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_7", parameters=parameters, response_types=response_types)
 
     return
 
