ad4c2dfcde721773a5c6d78f42cfc4596f7919bc
admin <root@localhost>
2019-08-19 17:56:12 +0000

    Saved: testing

--- Recon_Playbook.py
+++ Recon_Playbook.py
@@ -314,12 +314,11 @@ def prompt_5(action=None, success=None, container=None, results=None, handle=Non
     
     # set user and message variables for phantom.prompt call
     user = "admin"
-    message = """{0}{1}"""
+    message = """{0}"""
 
     # parameter list for template variable replacement
     parameters = [
         "file_reputation_1:action_result.data.*.response_code",
-        "file_reputation_1:action_result.data.*.scans.*.result",
     ]
 
     #responses:
