8b5c11055130c1d47ac3b42e0b587aa06259bcf8
admin <root@localhost>
2019-09-03 18:24:28 +0000

    Created URL_Recon

---
+++ URL_Recon.py
@@ -0,0 +1,279 @@
+"""
+"""
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+##############################
+# Start - Global Code Block
+
+import sys
+import urllib 
+import urlparse
+import time
+
+# End - Global Code block
+##############################
+
+def on_start(container):
+    phantom.debug('on_start() called')
+    
+    # call 'filter_1' block
+    filter_1(container=container)
+
+    return
+
+def Parsed_Proofpoint_URL(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('Parsed_Proofpoint_URL() called')
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.requestURL', 'artifact:*.id'])
+    container_item_0 = [item[0] for item in container_data]
+
+    Parsed_Proofpoint_URL__url_parsed = None
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+    test = container_item_0[0]
+        
+    query  = urlparse.urlparse(test).query
+    param  = urlparse.parse_qs(query)
+    u = (param['u'][0].replace('-', '%')
+                      .replace('_', '/'))
+    Parse_Proofpoint_URL__url_parsed = urllib.unquote(u)
+    phantom.debug(Parse_Proofpoint_URL__url_parsed)
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.save_run_data(key='Parsed_Proofpoint_URL:url_parsed', value=json.dumps(Parsed_Proofpoint_URL__url_parsed))
+    url_reputation_1(container=container)
+
+    return
+
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_1() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["https://urldefense.proofpoint.com", "in", "artifact:*.cef.requestURL"],
+        ],
+        name="filter_1:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        Parsed_Proofpoint_URL(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["https://urldefense.proofpoint.com", "not in", "artifact:*.cef.requestURL"],
+        ],
+        name="filter_1:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        url_reputation_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def url_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('url_reputation_1() called')
+
+    Parsed_Proofpoint_URL__url_parsed = json.loads(phantom.get_run_data(key='Parsed_Proofpoint_URL:url_parsed'))
+    # collect data for 'url_reputation_1' call
+
+    parameters = []
+    
+    # build parameters list for 'url_reputation_1' call
+    parameters.append({
+        'url': Parsed_Proofpoint_URL__url_parsed,
+    })
+
+    phantom.act("url reputation", parameters=parameters, assets=['phishtank'], callback=filter_2, name="url_reputation_1")
+
+    return
+
+def url_reputation_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('url_reputation_2() called')
+
+    # collect data for 'url_reputation_2' call
+    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_2:artifact:*.cef.requestURL', 'filtered-data:filter_1:condition_2:artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'url_reputation_2' call
+    for filtered_artifacts_item_1 in filtered_artifacts_data_1:
+        if filtered_artifacts_item_1[0]:
+            parameters.append({
+                'url': filtered_artifacts_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': filtered_artifacts_item_1[1]},
+            })
+
+    phantom.act("url reputation", parameters=parameters, assets=['phishtank'], callback=filter_3, name="url_reputation_2")
+
+    return
+
+def filter_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_2() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_1:action_result.summary.Verified", "==", True],
+        ],
+        name="filter_2:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_1:action_result.summary.Verified", "==", None],
+        ],
+        name="filter_2:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        detonate_url_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def detonate_url_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('detonate_url_1() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    Parsed_Proofpoint_URL__url_parsed = json.loads(phantom.get_run_data(key='Parsed_Proofpoint_URL:url_parsed'))
+    # collect data for 'detonate_url_1' call
+
+    parameters = []
+    
+    # build parameters list for 'detonate_url_1' call
+    parameters.append({
+        'url': Parsed_Proofpoint_URL__url_parsed,
+    })
+
+    phantom.act("detonate url", parameters=parameters, assets=['virustotal_api'], callback=filter_4, name="detonate_url_1")
+
+    return
+
+def filter_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_3() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_2:action_result.summary.Verified", "==", True],
+        ],
+        name="filter_3:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        call_api_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["url_reputation_2:action_result.summary.Verified", "==", None],
+        ],
+        name="filter_3:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        detonate_url_3(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+def detonate_url_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('detonate_url_3() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'detonate_url_3' call
+    inputs_data_1 = phantom.collect2(container=container, datapath=['url_reputation_2:artifact:*.cef.requestURL', 'url_reputation_2:artifact:*.id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'detonate_url_3' call
+    for inputs_item_1 in inputs_data_1:
+        if inputs_item_1[0]:
+            parameters.append({
+                'url': inputs_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': inputs_item_1[1]},
+            })
+
+    phantom.act("detonate url", parameters=parameters, assets=['virustotal_api'], callback=filter_5, name="detonate_url_3")
+
+    return
+
+def filter_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_4() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["detonate_url_1:action_result.data.*.positives", ">=", "1"],
+        ],
+        name="filter_4:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
+
+    return
+
+def filter_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_5() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["detonate_url_3:action_result.data.*.positives", ">=", "1"],
+        ],
+        name="filter_5:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
+
+    return
+
+def call_api_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('call_api_1() called')
+
+    return
+
+def on_finish(container, summary):
+    phantom.debug('on_finish() called')
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all detals of actions 
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    return
