1965e431418d4e7d9d8f5250a3d0e4e3515957f0
admin <root@localhost>
2019-09-05 14:24:15 +0000

    added Hash_Recon playbook

--- Recon_Playbook.py
+++ Recon_Playbook.py
@@ -42,162 +42,6 @@ def on_start(container):
 
     return
 
-def file_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('file_reputation_1() called')
-
-    # collect data for 'file_reputation_1' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.fileHashSha256', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'file_reputation_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'hash': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("file reputation", parameters=parameters, assets=['virustotal_api'], callback=File_Filter, name="file_reputation_1")
-
-    return
-
-def detonate_file_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('detonate_file_2() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'detonate_file_2' call
-    inputs_data_1 = phantom.collect2(container=container, datapath=['file_reputation_1:artifact:*.cef.cs6', 'file_reputation_1:artifact:*.id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'detonate_file_2' call
-    for inputs_item_1 in inputs_data_1:
-        if inputs_item_1[0]:
-            parameters.append({
-                'vault_id': inputs_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': inputs_item_1[1]},
-            })
-
-    phantom.act("detonate file", parameters=parameters, assets=['virustotal_api'], callback=filter_8, name="detonate_file_2")
-
-    return
-
-def File_Filter(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('File_Filter() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["file_reputation_1:action_result.data.*.response_code", "==", 0],
-        ],
-        name="File_Filter:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        detonate_file_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    # collect filtered artifact ids for 'if' condition 2
-    matched_artifacts_2, matched_results_2 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["file_reputation_1:action_result.data.*.positives", ">=", "5"],
-        ],
-        name="File_Filter:condition_2")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_2 or matched_results_2:
-        prompt_3(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
-
-    return
-
-def prompt_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('prompt_3() called')
-    
-    # set user and message variables for phantom.prompt call
-    user = "admin"
-    message = """Block Hash?
-{0}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "file_reputation_1:action_result.data.*.positives",
-    ]
-
-    #responses:
-    response_types = [
-        {
-            "prompt": "",
-            "options": {
-                "type": "list",
-                "choices": [
-                    "Yes",
-                    "No",
-                ]
-            },
-        },
-    ]
-
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_3", parameters=parameters, response_types=response_types, callback=join_prompt_5)
-
-    return
-
-def prompt_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('prompt_4() called')
-    
-    # set user and message variables for phantom.prompt call
-    user = "admin"
-    message = """Detonated
-Block Hash?
-{0}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "detonate_file_2:action_result.data.*.positives",
-    ]
-
-    #responses:
-    response_types = [
-        {
-            "prompt": "",
-            "options": {
-                "type": "list",
-                "choices": [
-                    "Yes",
-                    "No",
-                ]
-            },
-        },
-    ]
-
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_4", parameters=parameters, response_types=response_types, callback=join_prompt_5)
-
-    return
-
-def filter_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('filter_8() called')
-
-    # collect filtered artifact ids for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        action_results=results,
-        conditions=[
-            ["detonate_file_2:action_result.data.*.positives", ">=", "5"],
-        ],
-        name="filter_8:condition_1")
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        prompt_4(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
 def prompt_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     phantom.debug('prompt_5() called')
     
@@ -221,17 +65,6 @@ def prompt_5(action=None, success=None, container=None, results=None, handle=Non
 
     phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_5", response_types=response_types, callback=filter_9)
 
-    return
-
-def join_prompt_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('join_prompt_5() called')
-
-    # check if all connected incoming actions are done i.e. have succeeded or failed
-    if phantom.actions_done([ 'prompt_4', 'prompt_3', 'prompt_9' ]):
-        
-        # call connected block "prompt_5"
-        prompt_5(container=container, handle=handle)
-    
     return
 
 def filter_9(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
@@ -325,7 +158,7 @@ def prompt_9(action=None, success=None, container=None, results=None, handle=Non
         },
     ]
 
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_9", response_types=response_types, callback=join_prompt_5)
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_9", response_types=response_types, callback=prompt_5)
 
     return
 
@@ -436,12 +269,16 @@ def filter_20(action=None, success=None, container=None, results=None, handle=No
         container=container,
         conditions=[
             ["artifact:*.cef.fileHashSha256", ">=", "1"],
+            ["artifact:*.cef.fileHashMd5", ">=", "1"],
+            ["artifact:*.cef.fileHashSha1", ">=", "1"],
+            ["artifact:*.cef.fileHashSha512", ">=", "1"],
         ],
+        logical_operator='or',
         name="filter_20:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        file_reputation_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        playbook_SOAR_Hash_Recon_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
@@ -450,6 +287,14 @@ def playbook_SOAR_URL_Recon_1(action=None, success=None, container=None, results
     
     # call playbook "SOAR/URL_Recon", returns the playbook_run_id
     playbook_run_id = phantom.playbook("SOAR/URL_Recon", container=container, name="playbook_SOAR_URL_Recon_1")
+
+    return
+
+def playbook_SOAR_Hash_Recon_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('playbook_SOAR_Hash_Recon_1() called')
+    
+    # call playbook "SOAR/Hash_Recon", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("SOAR/Hash_Recon", container=container)
 
     return
 
